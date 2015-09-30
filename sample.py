f46e6500f7e17212d2521953737109c3b5fcc893
Vivek Prakash <vivek@hackerearth.com>
2015-09-30 15:00:24 +0530

    update input method in sample code

--- sample.py
+++ sample.py
@@ -1,14 +1,18 @@
 # This is a skeleton python code indicating how input and output should be
 # taken. You can write code in any language though.
 
-def read_mp3_files_from_current_dir():
-    # You should read all the mp3 files from the current directory where the
-    # program is being executed. You can read them one by one or get all files
-    # in one list.
-    return ['one.mp3', 'two.mp3']
+def read_input():
+    # input.txt contains the mp3 file name in each line which are present int
+    # he current directory the program is being executed.
+    f = open('input.txt')
+    files = f.read().split('\n')
+
+    # Sanity cleaning to remove empty strings
+    files = [f for f in files if f]
+    return files
 
 def process_mp3_files():
-    files = read_mp3_files_from_current_dir()
+    files = read_input()
     for mp3_file in files:
         print compute_emotion(mp3_file)
 
